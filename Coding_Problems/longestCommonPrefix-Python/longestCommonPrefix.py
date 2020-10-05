5dfef07847625a974f160dce8e6a076289dac13b
rexdivakar <rexdivakar@hotmail.com>
2020-10-05 11:03:15 +0530

    Added fix for flake8

--- Coding_Problems/longestCommonPrefix-Python/longestCommonPrefix.py
+++ Coding_Problems/longestCommonPrefix-Python/longestCommonPrefix.py
@@ -1,15 +1,18 @@
+from autopep8 import List
+
+
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        listLength = len(strs)
-        if listLength == 0:
+        listlength = len(strs)
+        if listlength == 0:
             return ""
-        elif listLength == 1:
+        elif listlength == 1:
             return strs[0]
         else:
             strs.sort(reverse=False)
-            minStringLength = min(len(strs[0]), len(strs[listLength - 1]))
+            minstringlength = min(len(strs[0]), len(strs[listlength - 1]))
             i = 0
-            while i < minStringLength and strs[0][i] == strs[listLength - 1][i]:
+            while i < minstringlength and strs[0][i] == strs[listlength - 1][i]:
                 i += 1
-            resultantPrefix = strs[0][0:i]
-            return resultantPrefix
+            resultantprefix = strs[0][0:i]
+            return resultantprefix
